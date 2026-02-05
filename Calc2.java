0a836c651f86745cf6c31737e688e188728d340c
omarch22 <146375360+omarch22@users.noreply.github.com>
2026-02-04 22:41:43 -0600

    Calc2.java

--- Calc2.java
+++ Calc2.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+//PathVaruable hace que los parametros sean necesarios 
 @RestController
 public class Calc {
 
