389857afe82e09cb7bfda4bdd8344919bc53cc5d
omarch22 <146375360+omarch22@users.noreply.github.com>
2026-02-04 22:22:53 -0600

    Add files via upload

---
+++ Calc.java
@@ -0,0 +1,58 @@
+package com.hii.app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Calc {
+
+    @RequestMapping("/")
+    public String root() {
+        return "Hola";
+    }
+
+    @GetMapping("/calc/{oper}/{n1}/{n2}")
+    public String calc(
+            @PathVariable String oper,
+            @PathVariable float n1,
+            @PathVariable float n2) {
+
+        float resultado=0;
+
+        if(oper.equals("suma")){
+            resultado=n1+n2;
+        }
+        else if(oper.equals("multiplicacion")){
+            resultado=n1*n2;
+        }
+        else if(oper.equals("division")){
+            resultado=n1/n2;
+        }
+
+       
+
+        return "Resultado: "+resultado;
+    }
+    
+
+    @GetMapping("/users/{userId}")
+    public String getUserDetails(
+            @PathVariable Long userId,
+            @RequestParam(value="name",defaultValue="World") String name) {
+
+        return "User ID: "+userId+", "+name;
+    }
+
+    @GetMapping("/hello")
+    public String hello(
+            @RequestParam(value="name",defaultValue="World") String name,
+            @RequestParam(value="carrera",defaultValue="World") String carrera) {
+
+        return "Hello "+name+"!, "+carrera;
+    }
+}
