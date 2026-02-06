073af8e2112930063459e8856de27122a7b6db87
omarch22 <146375360+omarch22@users.noreply.github.com>
2026-02-05 19:29:25 -0600

    returns Operacion.java

--- request-body-calc/src/main/java/com/request_body_calc/app/Operacion.java
+++ request-body-calc/src/main/java/com/request_body_calc/app/Operacion.java
@@ -1,3 +1,26 @@
+package com.request_body_calc.app;
+
 public class Operacion {
-    
-}
+
+    private String oper;
+    private float n1;
+    private float n2;
+
+    public Operacion() {
+    }
+
+    public String getOper() {
+        return oper;
+    }
+
+    public void setOper(String oper) {
+        this.oper = oper;
+    }
+
+    public float getN1() {
+        return n1;
+    }
+
+    public void setN1(float n1) {
+        this.n1 = n1;
+    }
