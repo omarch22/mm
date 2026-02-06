1970444d5824ac8fdf1cf4df061070dbecc66ace
omarch22 <146375360+omarch22@users.noreply.github.com>
2026-02-05 19:17:36 -0600

    Add files via upload

---
+++ request-body-calc/src/main/java/com/request_body_calc/app/RequestBodyCalcApplication.java
@@ -0,0 +1,13 @@
+package com.request_body_calc.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestBodyCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestBodyCalcApplication.class, args);
+	}
+
+}
---
+++ request-body-calc/src/test/java/com/request_body_calc/app/RequestBodyCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.request_body_calc.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestBodyCalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
