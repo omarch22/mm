c58906cfea516aad67865d6476130929a16b7854
omarch22 <146375360+omarch22@users.noreply.github.com>
2026-02-05 18:42:55 -0600

    Add files via upload

---
+++ calc(rquestparam)/src/main/java/com/hii/app/Calc.java
@@ -0,0 +1,38 @@
+package com.hii.app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Calc {
+
+    @RequestMapping("/")
+    public String root() {
+        return "Hola";
+    }
+
+    @GetMapping("/calc/{oper}/{n1}/{n2}")
+    public String calc(
+            @PathVariable String oper,
+            @PathVariable float n1,
+            @PathVariable float n2) {
+
+        float resultado=0;
+
+        if(oper.equals("suma")){
+            resultado=n1+n2;
+        }
+       
+
+       
+
+        return "Resultado: "+resultado;
+    }
+    
+
+}
